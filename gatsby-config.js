7ef9c4bf252405699a11c6dc3a115546c840ce88
Huy Ngo <huynvk@gmail.com>
2019-07-14 15:51:45 +0700

    Basic setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,41 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-alias-imports",
+      options: {
+        alias: {
+          "@src": "src",
+          "@pages": "src/pages",
+          "@components": "src/components",
+          "@utils": "src/utils",
+          "@constants": "src/constants",
+          "@templates": "src/templates",
+        },
+        extensions: [],
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.ts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Huy Ngo`,
+        short_name: `huynvk`,
+        start_url: `/`,
+        background_color: `#222`,
+        theme_color: `#FFFFFF`,
+        display: `minimal-ui`,
+        icon: `static/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
